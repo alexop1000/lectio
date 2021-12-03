b98a10bc6f05df515e3339b0a8f4355ea4535483
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2021-12-03 12:11:32 +0100

    Changed variable and class names
    Changed the name of various variables and a class,.
    Removed unnecessary console.log.

--- js/forside.js
+++ js/forside.js
@@ -3,20 +3,13 @@
 
         const skema = document.querySelector("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(2) section:nth-child(2)");
         const undervisning = document.querySelector("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(2) section:nth-child(1)");
-        const denRigtigeBoks = document.querySelector("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(1)")
+        const forsideBoks = document.querySelector("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(1)")
         const copiedSkema = skema.cloneNode(true);
         const copiedUndervisning = undervisning.cloneNode(true);
         undervisning.remove();
-        denRigtigeBoks.classList.add("sejBoks");
-        denRigtigeBoks.appendChild(skema);
-        denRigtigeBoks.appendChild(copiedUndervisning);
-        console.log(window.location.href);
-
-        for (let i = 0; i < 3; i++) {
-            
-        }
-
-
+        forsideBoks.classList.add("forsideBoks");
+        forsideBoks.appendChild(skema);
+        forsideBoks.appendChild(copiedUndervisning);
 
     }
 })()
