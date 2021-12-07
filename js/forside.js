e5de939291c4c292cfc994326978ccccb76420e0
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2021-12-07 22:40:29 +0100

    Quickfix attempt for forside
    attempt at fixing problem with forside layout at non 100% scaling/zoom

--- js/forside.js
+++ js/forside.js
@@ -3,13 +3,21 @@
 
         const skema = document.querySelector("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(2) section:nth-child(2)");
         const undervisning = document.querySelector("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(2) section:nth-child(1)");
-        const forsideBoks = document.querySelector("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(1)")
-        const copiedSkema = skema.cloneNode(true);
-        const copiedUndervisning = undervisning.cloneNode(true);
-        undervisning.remove();
-        forsideBoks.classList.add("forsideBoks");
-        forsideBoks.appendChild(skema);
-        forsideBoks.appendChild(copiedUndervisning);
+        const forsideBoks = document.querySelector("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(1)");
 
+        if (forsideBoks.clientHeight > 850) {
+            forsideBoks.classList.add("forsideZoomed");
+            forsideBoks.classList.remove("forsideNonzoomed");
+        }
+        else {
+            forsideBoks.classList.add("forsideNonzoomed");
+            forsideBoks.classList.remove("forsideZoomed");
+        }
+        forsideBoks.classList.add("forsideBoks");
+
+        undervisning.remove();
+        skema.remove();
+        forsideBoks.appendChild(skema);
+        forsideBoks.appendChild(undervisning);
     }
 })()
