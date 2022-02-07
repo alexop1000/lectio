4f4d0cfa5a36f3ebdfb380353ba7e5955cb522e5
Alex Op <46445843+alexop1000@users.noreply.github.com>
2022-02-07 10:48:28 +0100

    seje ting

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,10 @@
 chrome.storage.local.get(["checked"], (result) => {
     document.getElementById("autologin").checked = result.checked;
+    document.getElementById("fri").checked = result.checked;
 })
 document.getElementById("autologin").addEventListener("change", (e) => {
     chrome.storage.local.set({ checked: e.target.checked });
+})
+document.getElementById("fri").addEventListener("change", (e) => {
+    chrome.storage.local.set({ fricounter: e.target.checked });
 })
