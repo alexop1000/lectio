aaeff6f79ddc106eb5f70e1612ce5ff939c12497
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2022-02-08 15:23:01 +0100

    made some things togglable

--- js/alle.js
+++ js/alle.js
@@ -19,7 +19,6 @@ const cacheBackend = async (key, toGet, timeout) => {
 	});
 }
 
-
 const getBackend = async (key, toGet, timeout) => {
 	return new Promise(async resolve => {
 		if (!cachedValues[key]) {
@@ -44,65 +43,75 @@ const getBackend = async (key, toGet, timeout) => {
 	chrome.storage.local.get(["light"], (result) => {
 		if (result.light) document.body.classList.add("light-theme")
 	})
-	const fGetXML = async (skoleId, elevId) => {
-		const response = await getBackend("skemaEnd", async () => {
-			return new Promise(res2 => {
-				var xhttpUrl = "https://www.lectio.dk/lectio/" + skoleId + "/SkemaNy.aspx?type=elev&elevid=" + elevId;
-				console.log(xhttpUrl);
-				var xhttp = new XMLHttpRequest();
-				xhttp.onreadystatechange = function () {
-					if (this.readyState == 4 && this.status == 200) {
-						const skema = xhttp.responseXML.querySelector(".s2skema tbody tr:nth-child(4)");
-		
-						const currentDate = new Date()
-						const currentWeekday = new Date().getDay();
-						const skemaDay = skema.querySelector(`td:nth-child(${currentWeekday + 1})`);
-						const skemaBlock = skemaDay.querySelector("div");
-						let theEnd = new Date();
-		
-						const lectures = skemaBlock.querySelectorAll(".s2skemabrik");
-						for (const lecture of lectures) {
-							if (parseInt(xhttp.responseXML.querySelector(`.s2dayHeader td:nth-child(` + (currentWeekday + 1) + `)`).innerText.split("/")[0].match(/\d+/g)[0]) !== new Date().getDate()) continue;
-							//   const start = lecture.getAttribute("data-additionalinfo").match(/\d+:\d+/)[0]
-							const end = lecture.getAttribute("data-additionalinfo").match(/\d+:\d+/g).pop()
-							const endTime = new Date(
-								currentDate.getFullYear(),
-								currentDate.getMonth(),
-								currentDate.getDate(),
-								end.split(":")[0],
-								end.split(":")[1]
-							).getTime();
-							theEnd = endTime
+	chrome.storage.local.get(["fri"], (result) => {
+		if (result.fri) {
+			const fGetXML = async (skoleId, elevId) => {
+				const response = await getBackend("skemaEnd", async () => {
+					return new Promise(res2 => {
+						var xhttpUrl = "https://www.lectio.dk/lectio/" + skoleId + "/SkemaNy.aspx?type=elev&elevid=" + elevId;
+						console.log(xhttpUrl);
+						var xhttp = new XMLHttpRequest();
+						xhttp.onreadystatechange = function () {
+							if (this.readyState == 4 && this.status == 200) {
+								const skema = xhttp.responseXML.querySelector(".s2skema tbody tr:nth-child(4)");
+				
+								const currentDate = new Date()
+								const currentWeekday = new Date().getDay();
+								const skemaDay = skema.querySelector(`td:nth-child(${currentWeekday + 1})`);
+								const skemaBlock = skemaDay.querySelector("div");
+								let theEnd = new Date();
+				
+								const lectures = skemaBlock.querySelectorAll(".s2skemabrik");
+								for (const lecture of lectures) {
+									if (parseInt(xhttp.responseXML.querySelector(`.s2dayHeader td:nth-child(` + (currentWeekday + 1) + `)`).innerText.split("/")[0].match(/\d+/g)[0]) !== new Date().getDate()) continue;
+									//   const start = lecture.getAttribute("data-additionalinfo").match(/\d+:\d+/)[0]
+									const end = lecture.getAttribute("data-additionalinfo").match(/\d+:\d+/g).pop()
+									const endTime = new Date(
+										currentDate.getFullYear(),
+										currentDate.getMonth(),
+										currentDate.getDate(),
+										end.split(":")[0],
+										end.split(":")[1]
+									).getTime();
+									theEnd = endTime
+								}
+								res2(theEnd)
+							}
 						}
-						res2(theEnd)
+						xhttp.open("GET", xhttpUrl, true);
+						xhttp.responseType = "document";
+						xhttp.send();
+					})
+				}, 1000 * 60 * 60)
+				const header = document.querySelector("header[role='banner']");
+				header.innerHTML += `<p class="fricount">Du har fri om <strong id="fritid"></strong> <p>`;
+				setInterval(() => {
+					const timeUntilDate = response - (new Date())
+					let htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60 * 60 * 24))} dage, ${Math.floor(timeUntilDate / (1000 * 60 * 60) % 24)} timer og ${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter`;
+					if (timeUntilDate < 1000 * 60 * 60 * 24) {
+						htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60 * 60))} timer, ${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter og ${Math.floor(timeUntilDate / 1000 % 60)} sekunder`;
 					}
-				}
-				xhttp.open("GET", xhttpUrl, true);
-				xhttp.responseType = "document";
-				xhttp.send();
-			})
-		}, 1000 * 60 * 60)
-		const header = document.querySelector("header[role='banner']");
-		header.innerHTML += `<p class="fricount">Du har fri om <strong id="fritid"></strong> <p>`;
-		setInterval(() => {
-			const timeUntilDate = response - (new Date())
-			let htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60 * 60 * 24))} dage, ${Math.floor(timeUntilDate / (1000 * 60 * 60) % 24)} timer og ${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter`;
-			if (timeUntilDate < 1000 * 60 * 60 * 24) {
-				htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60 * 60))} timer, ${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter og ${Math.floor(timeUntilDate / 1000 % 60)} sekunder`;
+					if (timeUntilDate < 1000 * 60 * 60) {
+						htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter og ${Math.floor(timeUntilDate / 1000 % 60)} sekunder`;
+					}
+					document.getElementById("fritid").innerText = htmlToApply;
+				}, 1000)
 			}
-			if (timeUntilDate < 1000 * 60 * 60) {
-				htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter og ${Math.floor(timeUntilDate / 1000 % 60)} sekunder`;
-			}
-			document.getElementById("fritid").innerText = htmlToApply;
-		}, 1000)
-	}
-
-	const location = window.location.href;
-	fGetXML(location?.split("/")?.[4], location?.split("?elevid=")?.[1]?.split("&")[0] ?? location?.split("&elevid=")?.[1]?.split("&")?.[0]);
-	setInterval(() => {
-		const afkAlert = document.querySelector("div.ui-dialog.ui-corner-all.ui-widget")
-		if (afkAlert != null){
-			window.location.reload()
+		
+			const location = window.location.href;
+			fGetXML(location?.split("/")?.[4], location?.split("?elevid=")?.[1]?.split("&")[0] ?? location?.split("&elevid=")?.[1]?.split("&")?.[0]);
 		}
-	}, 5000)
+	})
+
+	chrome.storage.local.get(["antiAFK"], (result) => {
+		if (result.anyiAFK) {
+			setInterval(() => {
+				const afkAlert = document.querySelector("div.ui-dialog.ui-corner-all.ui-widget")
+				if (afkAlert != null){
+					window.location.reload()
+				}
+			}, 5000)
+		}
+	})
+
 })()
--- popup/popup.js
+++ popup/popup.js
@@ -4,9 +4,21 @@ chrome.storage.local.get(["checked"], (result) => {
 chrome.storage.local.get(["light"], (result) => {
     document.getElementById("light").checked = result.light;
 })
+chrome.storage.local.get(["fri"], (result) => {
+    document.getElementById("fri").checked = result.fri;
+})
+chrome.storage.local.get(["antiAFK"], (result) => {
+    document.getElementById("antiAFK").checked = result.antiAFK;
+})
 document.getElementById("autologin").addEventListener("change", (e) => {
     chrome.storage.local.set({ checked: e.target.checked });
 })
 document.getElementById("light").addEventListener("change", (e) => {
     chrome.storage.local.set({ light: e.target.checked });
+})
+document.getElementById("fri").addEventListener("change", (e) => {
+    chrome.storage.local.set({ fri: e.target.checked });
+})
+document.getElementById("antiAFK").addEventListener("change", (e) => {
+    chrome.storage.local.set({ antiAFK: e.target.checked });
 })
