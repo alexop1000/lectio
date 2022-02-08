0e46ac76daeb761a409fc7c6e913ece861a62e5b
Alex Op <46445843+alexop1000@users.noreply.github.com>
2022-02-08 09:06:01 +0100

    beskeder

--- js/alle.js
+++ js/alle.js
@@ -41,56 +41,56 @@ const getBackend = async (key, toGet, timeout) => {
 (async () => {
 
 	const fGetXML = async (skoleId, elevId) => {
-			const response = await getBackend("skemaEnd", async () => {
-				return new Promise(res2 => {
-					var xhttpUrl = "https://www.lectio.dk/lectio/" + skoleId + "/SkemaNy.aspx?type=elev&elevid=" + elevId;
-					console.log(xhttpUrl);
-					var xhttp = new XMLHttpRequest();
-					xhttp.onreadystatechange = function () {
-						if (this.readyState == 4 && this.status == 200) {
-							const skema = xhttp.responseXML.querySelector(".s2skema tbody tr:nth-child(4)");
-			
-							const currentDate = new Date()
-							const currentWeekday = new Date().getDay();
-							const skemaDay = skema.querySelector(`td:nth-child(${currentWeekday + 1})`);
-							const skemaBlock = skemaDay.querySelector("div");
-							let theEnd = new Date();
-			
-							const lectures = skemaBlock.querySelectorAll(".s2skemabrik");
-							for (const lecture of lectures) {
-								if (parseInt(xhttp.responseXML.querySelector(`.s2dayHeader td:nth-child(` + (currentWeekday + 1) + `)`).innerText.split("/")[0].match(/\d+/g)[0]) !== new Date().getDate()) continue;
-								//   const start = lecture.getAttribute("data-additionalinfo").match(/\d+:\d+/)[0]
-								const end = lecture.getAttribute("data-additionalinfo").match(/\d+:\d+/g).pop()
-								const endTime = new Date(
-									currentDate.getFullYear(),
-									currentDate.getMonth(),
-									currentDate.getDate(),
-									end.split(":")[0],
-									end.split(":")[1]
-								).getTime();
-								theEnd = endTime
-							}
-							res2(theEnd)
+		const response = await getBackend("skemaEnd", async () => {
+			return new Promise(res2 => {
+				var xhttpUrl = "https://www.lectio.dk/lectio/" + skoleId + "/SkemaNy.aspx?type=elev&elevid=" + elevId;
+				console.log(xhttpUrl);
+				var xhttp = new XMLHttpRequest();
+				xhttp.onreadystatechange = function () {
+					if (this.readyState == 4 && this.status == 200) {
+						const skema = xhttp.responseXML.querySelector(".s2skema tbody tr:nth-child(4)");
+		
+						const currentDate = new Date()
+						const currentWeekday = new Date().getDay();
+						const skemaDay = skema.querySelector(`td:nth-child(${currentWeekday + 1})`);
+						const skemaBlock = skemaDay.querySelector("div");
+						let theEnd = new Date();
+		
+						const lectures = skemaBlock.querySelectorAll(".s2skemabrik");
+						for (const lecture of lectures) {
+							if (parseInt(xhttp.responseXML.querySelector(`.s2dayHeader td:nth-child(` + (currentWeekday + 1) + `)`).innerText.split("/")[0].match(/\d+/g)[0]) !== new Date().getDate()) continue;
+							//   const start = lecture.getAttribute("data-additionalinfo").match(/\d+:\d+/)[0]
+							const end = lecture.getAttribute("data-additionalinfo").match(/\d+:\d+/g).pop()
+							const endTime = new Date(
+								currentDate.getFullYear(),
+								currentDate.getMonth(),
+								currentDate.getDate(),
+								end.split(":")[0],
+								end.split(":")[1]
+							).getTime();
+							theEnd = endTime
 						}
+						res2(theEnd)
 					}
-					xhttp.open("GET", xhttpUrl, true);
-					xhttp.responseType = "document";
-					xhttp.send();
-				})
-			}, 1000 * 60 * 60)
-			const header = document.querySelector("header[role='banner']");
-			header.innerHTML += `<p class="fricount">Du har fri om <strong id="fritid"></strong> <p>`;
-			setInterval(() => {
-				const timeUntilDate = response - (new Date())
-				let htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60 * 60 * 24))} dage, ${Math.floor(timeUntilDate / (1000 * 60 * 60) % 24)} timer og ${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter`;
-				if (timeUntilDate < 1000 * 60 * 60 * 24) {
-					htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60 * 60))} timer, ${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter og ${Math.floor(timeUntilDate / 1000 % 60)} sekunder`;
 				}
-				if (timeUntilDate < 1000 * 60 * 60) {
-					htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter og ${Math.floor(timeUntilDate / 1000 % 60)} sekunder`;
-				}
-				document.getElementById("fritid").innerText = htmlToApply;
-			}, 1000)
+				xhttp.open("GET", xhttpUrl, true);
+				xhttp.responseType = "document";
+				xhttp.send();
+			})
+		}, 1000 * 60 * 60)
+		const header = document.querySelector("header[role='banner']");
+		header.innerHTML += `<p class="fricount">Du har fri om <strong id="fritid"></strong> <p>`;
+		setInterval(() => {
+			const timeUntilDate = response - (new Date())
+			let htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60 * 60 * 24))} dage, ${Math.floor(timeUntilDate / (1000 * 60 * 60) % 24)} timer og ${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter`;
+			if (timeUntilDate < 1000 * 60 * 60 * 24) {
+				htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60 * 60))} timer, ${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter og ${Math.floor(timeUntilDate / 1000 % 60)} sekunder`;
+			}
+			if (timeUntilDate < 1000 * 60 * 60) {
+				htmlToApply = `${Math.floor(timeUntilDate / (1000 * 60) % 60)} minutter og ${Math.floor(timeUntilDate / 1000 % 60)} sekunder`;
+			}
+			document.getElementById("fritid").innerText = htmlToApply;
+		}, 1000)
 	}
 
 	const location = window.location.href;
