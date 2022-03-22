3373a774dcf8a67766841ee7896ecfb4981fbfdf
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2022-03-22 08:40:27 +0100

    forside reverted

--- js/forside.js
+++ js/forside.js
@@ -1,21 +1,21 @@
 pages.forside = (async () => {
-    const skema = await first("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(2) section:nth-child(2)");
-    const undervisning = await first("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(2) section:nth-child(1)");
-    const forsideBoks = await first("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(1)");
+    // const skema = await first("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(2) section:nth-child(2)");
+    // const undervisning = await first("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(2) section:nth-child(1)");
+    // const forsideBoks = await first("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(1)");
 
-    if (forsideBoks.clientHeight > 850) {
-        forsideBoks.classList.add("forsideZoomed");
-        forsideBoks.classList.remove("forsideNonzoomed");
-    }
-    else {
-        forsideBoks.classList.add("forsideNonzoomed");
-        forsideBoks.classList.remove("forsideZoomed");
-    }
-    forsideBoks.classList.add("forsideBoks");
+    // if (forsideBoks.clientHeight > 850) {
+    //     forsideBoks.classList.add("forsideZoomed");
+    //     forsideBoks.classList.remove("forsideNonzoomed");
+    // }
+    // else {
+    //     forsideBoks.classList.add("forsideNonzoomed");
+    //     forsideBoks.classList.remove("forsideZoomed");
+    // }
+    // forsideBoks.classList.add("forsideBoks");
 
-    undervisning.remove();
-    skema.remove();
-    forsideBoks.appendChild(skema);
-    forsideBoks.appendChild(undervisning);
+    // undervisning.remove();
+    // skema.remove();
+    // forsideBoks.appendChild(skema);
+    // forsideBoks.appendChild(undervisning);
 
 })
