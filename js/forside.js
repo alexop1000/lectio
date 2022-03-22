0a720c789f11f8f0b2feb35c9c759bb6d82b4576
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2022-03-22 08:36:30 +0100

    Revert "Changed forsideboks"
    This reverts commit 57cd0f6658eba204f22445b42b258d51e2a740bb.

--- js/forside.js
+++ js/forside.js
@@ -3,7 +3,14 @@ pages.forside = (async () => {
     const undervisning = await first("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(2) section:nth-child(1)");
     const forsideBoks = await first("#s_m_outerContentFrameDiv div:nth-child(3) div:nth-child(1)");
 
-    forsideBoks.classList.remove("forsideZoomed");
+    if (forsideBoks.clientHeight > 850) {
+        forsideBoks.classList.add("forsideZoomed");
+        forsideBoks.classList.remove("forsideNonzoomed");
+    }
+    else {
+        forsideBoks.classList.add("forsideNonzoomed");
+        forsideBoks.classList.remove("forsideZoomed");
+    }
     forsideBoks.classList.add("forsideBoks");
 
     undervisning.remove();
